b52744a0ac10155e4e8bff26ff9b1210a2060fff
MichaelAharoni <michaelaharoni001@gmail.com>
2022-04-29 12:39:53 +0300

    test

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { store } from './store/store'
 import "./styles/styles.scss";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 ReactDOM.render(
@@ -17,6 +18,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
